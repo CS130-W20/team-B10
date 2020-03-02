c2bcbb22dab91b281f94da8daf1c0a185682dd7c
jsicheng <46518782+jsicheng@users.noreply.github.com>
2020-03-01 16:55:20 -0800

    Added calendar template

--- maps/urls.py
+++ maps/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='site-home'),
     path('survey/', views.survey, name='site-survey'),
+    path('display/', views.display, name='site-display'),
     path('maps/', views.maps, name='site-maps'),
     path('about/', views.about, name='site-about'),
 ]
--- maps/views.py
+++ maps/views.py
@@ -9,5 +9,8 @@ def maps(request):
 def survey(request):
     return render(request, 'maps/survey.html')
 
+def display(request):
+    return render(request, 'maps/display.html')
+
 def about(request):
     return render(request, 'maps/about.html', {'title': 'About'})
