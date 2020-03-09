624f5f0db0bb6201fead96304da9534ba78cc22b
jsicheng <46518782+jsicheng@users.noreply.github.com>
2020-03-08 18:15:57 -0700

    Updated display to read from view

--- maps/views.py
+++ maps/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import json
 
 def home(request):
     return render(request, 'maps/home.html')
@@ -10,7 +11,12 @@ def survey(request):
     return render(request, 'maps/survey.html')
 
 def display(request):
-    return render(request, 'maps/display.html')
+    events_list = []
+    event1 = {'title': 'Testing', 'start':'2020-03-14T11:00:00', 'start':'2020-03-14T12:00:00'}
+    event2 = {'title': 'Test2', 'start':'2020-03-16T11:00:00', 'start':'2020-03-16T12:00:00'}
+    events_list.append(event1)
+    events_list.append(event2)
+    return render(request, 'maps/display.html', {'itinerary_in_json': json.dumps(events_list)})
 
 def about(request):
     return render(request, 'maps/about.html', {'title': 'About'})
