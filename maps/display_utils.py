276b583383426a268fdb28e413b0e450a9ee3f15
thegajan <thegajannagaraj@gmail.com>
2020-03-08 23:50:24 -0700

    multi day scheduling

--- maps/display_utils.py
+++ maps/display_utils.py
@@ -1,11 +1,20 @@
 from collections import namedtuple
-from .filter_scores import get_cluster_id_list, cluster_attraction_list
+from .filter_scores import get_cluster_id_list, cluster_attraction_list, get_dists, filter_attraction_list
 import datetime
 
 
 attr_info = namedtuple('attr_info', ['name','location'])
 Attraction = namedtuple('Attraction', ['places_id','lat','lon', 'score', 'is_restaurant'])
 Hyperparameters = namedtuple('Hyperparameters', ['num_iter', 'neighborhood_frac', 'bias', 'random_max', 'random_min', 'decay'])
+
+# filtering configurations
+num_iter = 5000
+neighborhood_frac = 1.5
+bias = 10
+random_max = 5
+random_min = 1
+decay = 5e-2
+hyperparameters = Hyperparameters(num_iter, neighborhood_frac, bias, random_max, random_min, decay)
 
 
 def days_between(d1, d2):
@@ -30,20 +39,23 @@ def attr_to_tup(attrs, is_rest=False):
 
 def clustering(num_days, have_breakfast, k_attractions, attraction_list):
     k_restaurants = 3 if have_breakfast else 2
-    # filtering configurations
-    num_iter = 5000
-    neighborhood_frac = 1.5
-    bias = 10
-    random_max = 5
-    random_min = 1
-    decay = 5e-2
-    hyperparameters = Hyperparameters(num_iter, neighborhood_frac, bias, random_max, random_min, decay)
-
     # generate cluster by number of days
     cluster_id_list, centroids = get_cluster_id_list(attraction_list, num_days)
     attraction_list_clusters = cluster_attraction_list(cluster_id_list, attraction_list, num_days)
 
     return attraction_list_clusters, centroids
+
+
+def scheduling(attraction_list_clusters, k_attractions, k_restaurants, hotel, hours):
+    schedules = []
+    for cluster in attraction_list_clusters:
+        L = get_dists(cluster)
+        l_avg = np.mean(L)
+        # curate the score list of each cluster
+        S = filter_attraction_list(cluster, L, l_avg, k_attractions, k_restaurants, hyperparameters)
+        compile_schedule, tpa, tpr = scheduler(S, L, cluster, k_attractions, k_restaurants, hotel, hours)
+        schedules.append((compile_schedule, tpa, tpr))
+    return schedules
 
 
 def format_schedule(start_date, end_date, attractions, schedule, tpa, tpr, hotel_name, wake, sleep):
